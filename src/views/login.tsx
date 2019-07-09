4ec4f88080eda43f781f95a7ca71aee1ecb5c02b
mangoguang <542436458@qq.com>
2019-07-09 14:42:38 +0800

    引入导航

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,15 +1,29 @@
+
 import React, {Fragment} from 'react';
 import {
   View,
   Text,
+  StyleSheet,
 } from 'react-native';
 
 const Login = () => {
   return (
     <View>
-      <Text>登陆页面</Text>
+      <Text
+      style={styles.title}
+      >登陆页面</Text>
     </View>
   );
 };
+
+// type Style = {title: object}
+const styles = StyleSheet.create({
+  title: {
+    color: 'red',
+  },
+  content: {
+    color: 'blue',
+  }
+})
 
 export default Login;
