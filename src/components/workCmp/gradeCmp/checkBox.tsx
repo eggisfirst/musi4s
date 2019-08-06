6df729732c03b3254142395aab3b33b0fe310739
xiaowei <578850593@qq.com>
2019-08-06 17:13:07 +0800

    评分页面完成

---
+++ src/components/workCmp/gradeCmp/checkBox.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import pxToDp from "../../../utils/fixcss";
+
+
+interface IProps {
+  item: {
+    title: string
+    status: boolean
+  }
+  index?: number
+  handleToGrade?: (index: number) => void
+}
+
+
+export const CheckBox: React.FC<IProps> = ({item,index,handleToGrade}) => {
+  //跳转到评分页面
+  const handleClick = () => {
+    if(handleToGrade && index !== undefined) {
+      handleToGrade(index)
+    }
+  }
+  return(
+    <View style={styles.wrapper}>
+      <Text style={styles.content}>年度经验</Text>
+      {
+        item.status? 
+        <View style={styles.hadGradeBtn}>
+          <Image style={styles.icon} source={require('../../../images/work/grade/graded.png')} />
+          <Text style={styles.hadGradeText}>已评分</Text>
+        </View> :
+        <TouchableOpacity onPress={() => {handleClick()}} style={styles.btn}>
+          <Text style={styles.grade}>去评分>></Text>
+        </TouchableOpacity> 
+
+      }
+    </View>
+   )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: pxToDp(214),
+    height: pxToDp(174),
+    backgroundColor: "#f2f5f7",
+    borderRadius: pxToDp(12),
+    marginTop: pxToDp(22),
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: pxToDp(22)
+
+  },
+  content: {
+    fontSize: pxToDp(26),
+    color: "#363636",
+    // marginTop: pxToDp(20),
+    paddingLeft: pxToDp(37),
+    paddingRight: pxToDp(37),
+    textAlign: "center",
+    lineHeight: pxToDp(40)
+  },
+  btn: {
+    width: pxToDp(121),
+    height: pxToDp(44),
+    backgroundColor: "rgba(0,122,255,0.3)",
+    borderRadius: pxToDp(22),
+    marginTop: pxToDp(20)
+  },
+  hadGradeBtn: {
+    width: pxToDp(121),
+    height: pxToDp(44),
+    backgroundColor: "rgba(76,217,100,0.3)",
+    borderRadius: pxToDp(22),
+    marginTop: pxToDp(20),
+    display:"flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  hadGradeText: {
+    color: "#4CD964",
+    fontSize: pxToDp(23),
+    paddingLeft: pxToDp(5)
+  },
+  grade: {
+    fontSize: pxToDp(23),
+    color: "#007aff",
+    textAlign: "center",
+    lineHeight: pxToDp(44),
+  },
+  icon: {
+    width: pxToDp(24),
+    height: pxToDp(24)
+  }
+})
--- src/views/work/gradePage/grade.tsx
+++ src/views/work/gradePage/grade.tsx
@@ -3,17 +3,37 @@ import React from "react";
 import { View, Text, Platform, StatusBar, StyleSheet } from "react-native";
 import pxToDp from "../../../utils/fixcss";
 import { HeaderCmp } from '../../../components/workCmp/starCheck/headerCmp';
+import { CheckBox } from '../../../components/workCmp/gradeCmp/checkBox';
+
 
 
 export default class GradePage extends React.Component<any>{
   static navigationOptions = {
     header: null
   }
-  eggHandleBack = () => {
-
+  //跳转到评分页面
+  handleToGrade = (index: number) => {
+    console.log(index)
   }
   render (){
     const {navigation} = this.props
+    const list = [
+      {
+        status: false,
+        title: "年度经营计划"
+      },
+      {
+        status: true,
+        title: "员工形象"
+      },
+      {
+        status: true,
+        title: "店面环境"
+      }, {
+        status: false,
+        title: "年度经营计划"
+      }
+    ]
     return(
       <View>
         <HeaderCmp title={'五星认证评分'} eggHandleBack={() => {navigation.goBack()}}/>
@@ -21,7 +41,21 @@ export default class GradePage extends React.Component<any>{
           <Text style={styles.sum_title}>广州马会家居凯奇门店</Text>
           <Text style={styles.sum_text}>共<Text style={styles.sum_blue}>18</Text>项 已评<Text style={styles.sum_green}>5</Text>项 剩余 <Text style={styles.sum_red}>14</Text>项未评</Text>
         </View>
-        
+        <View style={styles.checkWrapper}>
+          <Text style={styles.start_check_title}>一星检查</Text>
+          <View style={styles.checkBox} >
+            {
+              list.map((item, index) => {
+                return (
+                  <CheckBox key={index} 
+                            item={item} 
+                            index={index} 
+                            handleToGrade={this.handleToGrade}/>
+                )
+              })
+            }
+          </View>
+        </View>
       </View>
       )
   }
@@ -56,5 +90,23 @@ const styles = StyleSheet.create({
   },
   sum_red: {
     color: "#FF2D55"
-  }
+  },
+  checkWrapper: {
+    paddingLeft: pxToDp(31),
+    paddingRight: pxToDp(10),
+    paddingTop: pxToDp(50),
+    paddingBottom: pxToDp(50),
+  },
+  start_check_title: {
+    fontSize: pxToDp(45),
+    fontWeight:"bold",
+    color: "#363636"
+  },
+  checkBox: {
+    display: "flex",
+    flexDirection:"row",
+    flexWrap: "wrap",
+    
+  },
+ 
 })
