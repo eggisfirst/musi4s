92df13afadd2534956524fa049001711d831b47c
xiaowei <578850593@qq.com>
2019-08-24 09:03:43 +0800

    8/24早

--- src/components/workCmp/areaReportCmp/checkDetailsCmp/swiper/static/index.js
+++ src/components/workCmp/areaReportCmp/checkDetailsCmp/swiper/static/index.js
@@ -1,18 +1,22 @@
 export const ENTRIES1 = [
-  {
-      illustration: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3893146502,314297687&fm=26&gp=0.jpg'
-  },
-  {
-      illustration: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=508387608,2848974022&fm=26&gp=0.jpg'
-  },
-  {
-      illustration: 'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg'
-  }
+    {
+        illustration: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3893146502,314297687&fm=26&gp=0.jpg'
+    },
+    {
+        illustration: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1563980539,1672265910&fm=26&gp=0.jpg'
+    },
+    {
+        illustration: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3987907653,720009510&fm=26&gp=0.jpg'
+    },
+    {
+        illustration:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3139953554,3011511497&fm=26&gp=0.jpg'
+    }
+
 ];
 
 export const ENTRIES2 = [
- 'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg',
- 'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg',
- 'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg',
- 'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg'
+    'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg',
+    'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg',
+    'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg',
+    'http://img2.imgtn.bdimg.com/it/u=3528640992,1418471262&fm=26&gp=0.jpg'
 ];
