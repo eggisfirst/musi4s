66935afbcc4d785f57524581ad3c4239c3fcdb11
xiaowei <578850593@qq.com>
2019-09-21 15:39:40 +0800

    1

--- src/components/common/imgUploadCmp.tsx
+++ src/components/common/imgUploadCmp.tsx
@@ -1,6 +1,6 @@
 
-import React, {Component} from 'react';
-import { 
+import React, { Component } from 'react';
+import {
   Text,
   Alert,
   StyleSheet,
@@ -8,7 +8,7 @@ import {
   Image,
   ImageBackground,
   TouchableOpacity,
-} from "react-native"; 
+} from "react-native";
 import pxToDp from '../../utils/fixcss';
 import ImagePicker from 'react-native-image-picker';
 import { IndexModel } from "../../request";
@@ -21,7 +21,7 @@ interface IState {
 }
 
 interface IProps {
-  getImageList:(obj:object) => void
+  getImageList: (obj: object) => void
   imageList: string[]
   openBigImageBox: () => void
   changeBigImage: (str: string) => void
@@ -31,7 +31,7 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
 
   state: IState = {
     // imageList: [],
-    avatarSource: {uri: '../../images/work/starCheck/addImg.png'},
+    avatarSource: { uri: '../../images/work/starCheck/addImg.png' },
   }
 
   _imgDataList: object[] = []
@@ -53,6 +53,8 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
     },
   };
 
+
+
   upload = () => {
     if (this.props.imageList.length >= 5) {
       Alert.alert(
@@ -61,7 +63,7 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
         [
           // {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
           // {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-          {text: '好的', onPress: () => console.log('onPress OK')},
+          { text: '好的', onPress: () => console.log('onPress OK') },
         ],
         { cancelable: false }
       )
@@ -75,11 +77,46 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
         } else if (response.customButton) {
           console.log('User tapped custom button: ', response.customButton);
         } else {
-          this.saveImage(response)
+          this.saveImage(response, 'image')
         }
       });
     }
   }
+
+  /**上传视频 */
+  uploadVideo = () => {
+    const options = {
+      title: '选择视频',
+      cancelButtonTitle: '取消',
+      takePhotoButtonTitle: '录制视频',
+      chooseFromLibraryButtonTitle: '选择视频',
+      mediaType: 'video',
+      videoQuality: 'high',
+      durationLimit: 10,
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled video picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        this.saveImage(response, 'video')
+      }
+    });
+  }
+
 
   /**
    * @param {*删除的图片下标} index
@@ -90,8 +127,8 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
       '确定删除图片吗？',
       [
         // {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-        {text: '取消', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        {text: '确定', onPress: () => this.sureCutImg(index)},
+        { text: '取消', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        { text: '确定', onPress: () => this.sureCutImg(index) },
       ],
       { cancelable: false }
     )
@@ -111,8 +148,14 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
   /**
    * @param {*上传的文件} formData
    */
-  saveImage = (response:any) => {
-    let file = {uri: response.uri, type: 'multipart/form-data', name: 'image.jpg'}
+  saveImage = (response: any, type: string) => {
+    let file;
+    if (type === 'video') {
+      file = { uri: response.uri, type: 'multipart/form-data', name: 'video.mp4' }
+    } else {
+      file = { uri: response.uri, type: 'multipart/form-data', name: 'image.jpg' }
+
+    }
     let formData = new FormData()
     // formData.append('dataFile', file)
     formData.append('multipartFile', file)
@@ -137,26 +180,26 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
     const imgBoxList = this.props.imageList.map((item: string, index) => {
       console.log(123, item)
       return <View
-              style={[styleSheet.imgBox, {marginRight: index === 2 ? pxToDp(0) : pxToDp(20)}]}
-              key={`imgBoxList${index}`}
-            >
-              <TouchableOpacity
-              onPress={() => this.cutImg(index)}
-                style={styleSheet.closeBtn}
-              >
-              <Image source={require('../../images/work/starCheck/cutImg.png')} style={styleSheet.cutImg} />
-              </TouchableOpacity>
-              <TouchableWithoutFeedback
-                onPress={() => this.watchBigImage(index)}
-              >
-                <Image source={{uri: item}} style={styleSheet.img} />
-              </TouchableWithoutFeedback>
-            </View>
+        style={[styleSheet.imgBox, { marginRight: index === 2 ? pxToDp(0) : pxToDp(20) }]}
+        key={`imgBoxList${index}`}
+      >
+        <TouchableOpacity
+          onPress={() => this.cutImg(index)}
+          style={styleSheet.closeBtn}
+        >
+          <Image source={require('../../images/work/starCheck/cutImg.png')} style={styleSheet.cutImg} />
+        </TouchableOpacity>
+        <TouchableWithoutFeedback
+          onPress={() => this.watchBigImage(index)}
+        >
+          <Image source={{ uri: item }} style={styleSheet.img} />
+        </TouchableWithoutFeedback>
+      </View>
     })
-    return(
+    return (
       <View style={styleSheet.imgUploadWrapper}>
         <View style={styleSheet.imgUploadWrapper}>
-          {imgBoxList}
+          {this.props.imageList.length && imgBoxList}
           {/* <Image source={this.state.avatarSource} style={{width: 200,height: 200}} /> */}
           <ImageBackground
             style={styleSheet.addBox}
@@ -166,7 +209,16 @@ export default class ImgUploadCmp extends Component<IProps, IState> {
               style={styleSheet.addBtn}
             ></TouchableOpacity>
           </ImageBackground>
-          
+
+          <ImageBackground
+            style={styleSheet.addBox}
+            source={require("../../images/work/starCheck/addImg.png")}>
+            <TouchableOpacity
+              onPress={() => { this.uploadVideo() }}
+              style={styleSheet.addBtn}
+            ></TouchableOpacity>
+          </ImageBackground>
+
         </View>
       </View>
     )
@@ -182,7 +234,7 @@ const styleSheet = StyleSheet.create({
     marginTop: pxToDp(100),
     paddingBottom: pxToDp(14),
   },
-  closeBtn:{
+  closeBtn: {
     position: 'absolute',
     top: 0,
     right: 0,
